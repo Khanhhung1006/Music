96de9049754fdcd85b9eec22dc6ce3515fc7ff12
Khanhhung1006 <136552632+Khanhhung1006@users.noreply.github.com>
2026-09-10 18:03:26 +0700

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.bandb.music',
+  appName: 'B and B Music',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
